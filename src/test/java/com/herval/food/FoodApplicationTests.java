c701b7b4c2f808f911636a72e9526dd0240ff7fb
HervalMata <hervalmataamparo@gmail.com>
2019-12-13 21:56:10 -0300

    Criando o projeto

---
+++ src/test/java/com/herval/food/FoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herval.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
